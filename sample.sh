c0beae817fbb611533a15c936342d8bca87386ec
abhinavthula <67160595+abhinavthula@users.noreply.github.com>
2022-07-19 13:26:27 +0530

    upadated sample script
    upadated sample script

--- sample.sh
+++ sample.sh
@@ -1,1 +1,4 @@
-
+for branch in 'git branch -a | grep remotes | grep -v HEAD | grep -v main'; do
+git branch --track $ {branch#remotes/origin/} $branch 
+done
+git log --shortstat --since "2022-07-18" --until "2022-07-19" --pretty="%cE" | sed 's/\(.*\)@.*/\1/' | grep -v "^$" | awk 'BEGIN { line=""; } !/^ / { if (line=="" || !match(line, $0)) {line = $0 "," line }} /^ / { print line " # " $0; line=""}' | sort | sed -E 's/# //;s/ files? changed,//;s/([0-9]+) ([0-9]+ deletion)/\1 0 insertions\(+\), \2/;s/\(\+\)$/\(\+\), 0 deletions\(-\)/;s/insertions?\(\+\), //;s/ deletions?\(-\)//' | awk 'BEGIN {name=""; files=0; insertions=0; deletions=0;} {if ($1 != name && name != "") { print name ": " files " files changed, " insertions " insertions(+), " deletions " deletions(-), " insertions-deletions " net"; files=0; insertions=0; deletions=0; name=$1; } name=$1; files+=$2; insertions+=$3; deletions+=$4} END {print name ": " files " files changed, " insertions " insertions(+), " deletions " deletions(-), " insertions-deletions " net";}'
